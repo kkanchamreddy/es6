364792248a428796fc966b87becb64044989e4ad
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-02-06 22:34:38 -0800

    Es6 style inheritance

--- classes/inheritance.js
+++ classes/inheritance.js
@@ -36,3 +36,31 @@ console.log('sqr instanceof Square: ', sqr instanceof Square);
 console.log('sqr instanceof Rectangle: ', sqr instanceof Rectangle);
 console.log('constructor', sqr.constructor);
 console.log('Area of square is: ', sqr.getArea());
+
+
+
+/******  ES6 Style *********/
+console.log('-------ES6-----------');
+class RectangleType {
+    constructor(length, width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    getArea() {
+        return this.length * this.width;
+    }
+}
+
+class SquareType extends RectangleType {
+    constructor(length) {
+        // same as Rectangle.call(this, length, length)
+        super(length, length);
+    }
+}
+
+var sqrType = new SquareType(10);
+console.log('sqr instanceof Square: ', sqrType instanceof SquareType);
+console.log('sqr instanceof Rectangle: ', sqrType instanceof RectangleType);
+console.log('constructor', sqrType.constructor);
+console.log('Area of square is: ', sqrType.getArea());

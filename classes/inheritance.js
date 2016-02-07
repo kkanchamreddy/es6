4f5b2b581be393f250a88679f257b0e516588aea
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-02-06 22:13:48 -0800

    Barebones es5 style inheritance

---
+++ classes/inheritance.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function Rectangle (length, width){
+  this.length = length;
+  this.width = width;
+}
+
+Rectangle.prototype.getArea = function() {
+  return this.length * this.width;
+};
+
+var rect = new Rectangle(10, 20);
+
+console.log('rect instanceof Rectangle: ', rect instanceof Rectangle);
+console.log('Area of rectangle is: ', rect.getArea());
+
+function Square(length, width) {
+  Rectangle.call(this, length, width);
+}
+
+Square.prototype = Rectangle.prototype;
+
+var sqr = new Square(10, 10);
+console.log('sqr instanceof Square: ', sqr instanceof Square);
+console.log('sqr instanceof Rectangle: ', sqr instanceof Rectangle);
+console.log('Area of square is: ', sqr.getArea());
